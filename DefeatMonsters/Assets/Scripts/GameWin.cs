4e818212d0467599db634148fd5f183b45ace071
NolanNgo <48820527+NolanNgo@users.noreply.github.com>
2022-01-02 19:49:23 +0700

    update

--- DefeatMonsters/Assets/Scripts/GameWin.cs
+++ DefeatMonsters/Assets/Scripts/GameWin.cs
@@ -18,7 +18,7 @@ public class GameWin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        data = SaveSystem.LoadPlayer();
+        
         // Debug.Log(playerData.dame);
         // Debug.Log(playerData.maxHealth);
         // id Items
--- DefeatMonsters/Assets/Scripts/PlayerController.cs
+++ DefeatMonsters/Assets/Scripts/PlayerController.cs
@@ -38,7 +38,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SaveSystem.SavePlayer(this);
         CoinsText.text = "COIN: " + coins.ToString();
         healthBar.fillAmount = healthPlayer / maxHealth;
         if (!chkDie)
